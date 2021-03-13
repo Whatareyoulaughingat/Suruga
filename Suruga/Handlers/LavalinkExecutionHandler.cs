efdc8ad2ed29057bc4ba3e2dbec0f3e98fddbcf1
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-03-13 23:17:18 +0200

    Created a way to search for the correct Java version before trying to execute Lavalink.

---
+++ Suruga/Handlers/LavalinkExecutionHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Suruga.Handlers
+{
+    public class LavalinkExecutionHandler
+    {
+        public static string SearchForJava()
+        {
+            IEnumerable<string> java = Directory.EnumerateFiles($"C:\\Program Files", "*java.exe", new EnumerationOptions()
+            {
+                AttributesToSkip = FileAttributes.Hidden | FileAttributes.Compressed | FileAttributes.System,
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true,
+            });
+
+            if (!java.Any())
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Could not find any Java version installed!");
+                Console.ResetColor();
+            }
+
+            return java.Where(x => x.Contains("jdk-13.0.2")).First();
+        }
+    }
+}
--- Suruga/Program.cs
+++ Suruga/Program.cs
@@ -10,35 +10,34 @@ namespace Suruga
     public class Program
     {
         /// <summary>
-        /// The main method where execution starts.
+        /// The main method where execution of this bot starts.
         /// </summary>
         /// <returns>[<see cref="Task"/>] An asynchronous operation.</returns>
         private static async Task Main()
         {
-            Console.Title = "Suruga v4";
+            Console.Title = "Suruga";
 
-            ConfigurationHandler configurationHandler = new ConfigurationHandler();
+            ConfigurationHandler configurationHandler = new();
             configurationHandler.SerializeConfiguration();
             configurationHandler.DeserializeConfiguration();
 
             RunLavalink();
-
             await new SurugaClient().RunAsync();
         }
 
         /// <summary>
-        /// Runs Lavalink and attaches its process to this bot so everytime this bot exits, Lavalink will exit too.
+        /// Runs Lavalink and attaches its process to this bot so everytime this bot exits, Lavalink will exit as well.
         /// <para></para>
         /// Assuming Java 13 (JRE) is installed on your system in its default location (C:\Program Files\Java\jdk-13.0.2) as well as the Lavalink.jar file is located in the same path as the executable file of this bot.
         /// </summary>
         private static void RunLavalink()
         {
-            using Process lavalink = new Process();
+            using Process lavalink = new();
             lavalink.StartInfo.CreateNoWindow = true;
             lavalink.StartInfo.UseShellExecute = true;
             lavalink.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            lavalink.StartInfo.FileName = "C:\\Program Files\\Java\\jdk-13.0.2\\bin\\java";
-            lavalink.StartInfo.Arguments = $"-jar {ProcessExtensions.EncodeParameterArgument($"C:\\Users\\{Environment.UserName}\\AppData\\Local\\Suruga\\Lavalink.jar")}";
+            lavalink.StartInfo.FileName = "\"" + LavalinkExecutionHandler.SearchForJava() + "\"";
+            lavalink.StartInfo.Arguments = "-jar Lavalink.jar";
             lavalink.Start();
 
             // Tracks the child process. If the parent process exits, the child does as well.
