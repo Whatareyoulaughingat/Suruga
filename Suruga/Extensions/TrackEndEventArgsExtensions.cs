0a34951548801c673c59800949a041f0db2b809e
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-04-29 11:16:57 +0300

    An extension method for converting lavalink players.

---
+++ Suruga/Extensions/TrackEndEventArgsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Lavalink4NET.Player;
+
+namespace Suruga.Extensions
+{
+    public static class TrackEndEventArgsExtensions
+    {
+        public static QueuedLavalinkPlayer ToQueuedLavalinkPlayer(this LavalinkPlayer player)
+        {
+            return _ = player as QueuedLavalinkPlayer;
+        }
+    }
+}
