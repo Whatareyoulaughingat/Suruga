d75b50f80f4b79fcf268eca2858dfd8dae4f802f
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-06-18 07:39:23 +0300

    Added a struct containing folder and file paths that are only going to be used by this bot.

---
+++ Suruga/GlobalData/Paths.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Suruga.GlobalData
+{
+    public struct Paths
+    {
+        public static readonly string Base = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\Suruga";
+
+        public static readonly string Configuration = $"{Base}\\Configuration.json";
+    }
+}
