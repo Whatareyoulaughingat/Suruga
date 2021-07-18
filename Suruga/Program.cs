824c4bb7c39ca26ed1583d1b29a5a9a85a47546d
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-07-18 21:32:32 +0300

    Added exception handling when trying to run Lavalink and used the new configuration variables for it.

--- Suruga/Program.cs
+++ Suruga/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Suruga.Client;
@@ -10,9 +11,6 @@ namespace Suruga
 {
     public class Program
     {
-        protected Program()
-            => RunLavalink();
-
         /// <summary>
         /// The main method where execution of this bot starts.
         /// </summary>
@@ -21,8 +19,12 @@ namespace Suruga
         {
             Console.Title = "Suruga";
 
-            await ConfigurationHandler.SerializeConfigurationAsync();
-            await ConfigurationHandler.DeserializeConfigurationAsync();
+            ConfigurationHandler configHandler = new();
+            await configHandler.SerializeAsync();
+            await configHandler.DeserializeAsync();
+
+            await RunLavalink();
+            await Task.Delay(TimeSpan.FromSeconds(int.Parse(ConfigurationHandler.Data.WaitForLavalinkToOpenInterval)));
 
             await new SurugaClient().RunAsync();
         }
@@ -32,18 +34,26 @@ namespace Suruga
         /// <para></para>
         /// Assuming Java 13 (JRE) is installed on your system in its default location (C:\Program Files\Java\jdk-13.0.2) as well as the Lavalink.jar file is located in the same path as the executable file of this bot.
         /// </summary>
-        private static void RunLavalink()
+        private static async Task RunLavalink()
         {
-            using Process lavalink = new();
-            lavalink.StartInfo.CreateNoWindow = true;
-            lavalink.StartInfo.UseShellExecute = true;
-            lavalink.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            lavalink.StartInfo.FileName = "\"" + LavalinkExecutionHandler.SearchForJava() + "\"";
-            lavalink.StartInfo.Arguments = "-jar Lavalink.jar";
-            lavalink.Start();
+            try
+            {
+                using Process lavalink = new();
+                lavalink.StartInfo.CreateNoWindow = true;
+                lavalink.StartInfo.UseShellExecute = true;
+                lavalink.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                lavalink.StartInfo.FileName = "\"" + LavalinkExecutionHandler.SearchForJavaAsync() + "\"";
+                lavalink.StartInfo.Arguments = $"-jar {ConfigurationHandler.Data.LavalinkFilePath}";
+                lavalink.Start();
 
-            // Tracks the child process. If the parent process exits, the child does as well.
-            new ProcessAttachmentHandler().AddProcessAsChild(lavalink);
+                // Tracks the child process. If the parent process exits, the child does as well.
+                new ProcessAttachmentHandler().AddProcessAsChild(lavalink);
+            }
+            catch (Win32Exception)
+            {
+                await Console.Out.WriteLineAsync("Could not find Lavalink. Place it where this application is or configure a custom file path in the congiguration file if you have it or download it from: https://github.com/freyacodes/Lavalink and follow the installation guide.");
+                await Task.Delay(-1).ConfigureAwait(false);
+            }
         }
     }
 }
