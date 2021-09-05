7a1558ae8405331477da5f082ee576dd3edde0d6
Nikos Naoum <nikosnaoumv2@outlook.com>
2021-09-05 22:55:53 +0300

    Solution and project rename.

--- Suruga/GlobalData/Paths.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Suruga.GlobalData;
-
-public static class Paths
-{
-    public static string Base => Path.GetFullPath("Suruga", Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
-
-    public static string Configuration => Path.GetFullPath("Configuration.json", Base);
-}
